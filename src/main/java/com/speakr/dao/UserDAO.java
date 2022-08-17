10748224e42e2b1ba9a7b93a46b807405cdffea3
Alonso del Arte <alonso.delarte@gmail.com>
2022-08-17 17:48:42 -0400

    Dealing with git diff mistake

--- src/main/java/com/speakr/dao/UserDAO.java
+++ src/main/java/com/speakr/dao/UserDAO.java
@@ -9,27 +9,14 @@ import java.util.Objects;
 
 @Repository
 public interface UserDAO extends JpaRepository<User, Integer> {
-<<<<<<< HEAD
 
     default User findUserByUserName(String user_name)
     {
         return this.findAll().stream()
-                .filter(user -> Objects.equals(user.getUser_name(), user_name))
+                .filter(user -> Objects.equals(user.getUserName(), user_name))
                 .reduce((a, b) -> { throw new IllegalStateException("Multiple users: " + a + ", " + b);
                 })
                 .orElse(null);
     }
 
-=======
-	
-	default User findUserByUserName(String user_name)
-	{
-		return this.findAll().stream()
-			.filter(user -> Objects.equals(user.getUser_name(), user_name))
-			.reduce((a, b) -> { throw new IllegalStateException("Multiple users: " + a + ", " + b);
-			})
-			.orElse(null);
-	}
-	
->>>>>>> 6e1ceb4b86e938fba730f32a878253ba68fdae79
 }
