2ac3dfadb60044e6271cc8bb58c72a78d8e34823
Alonso del Arte <alonso.delarte@gmail.com>
2022-08-17 17:50:25 -0400

    Dealing with git diff mistake

--- src/main/java/com/speakr/entity/Post.java
+++ src/main/java/com/speakr/entity/Post.java
@@ -1,8 +1,9 @@
 package com.speakr.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,34 +13,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "post_tbl")
-public class Post
-{
-<<<<<<< HEAD
+public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int post_id;
+    private int postId;
 
     @Column
     private String text;
 
     @Temporal(TemporalType.DATE)
     private Date postTime;
-
 
     @OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.PERSIST)
     @JoinColumn(name="parent_id")
@@ -51,121 +46,51 @@ public class Post
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getPostTime() {
+        return this.postTime;
+    }
+
+    public void setPostTime(Date postTime) {
+        this.postTime = postTime;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void addReply(Post post) {
+        this.replies.add(post);
+    }
+
+    public void removeReply(Post post) {
+        this.replies.remove(post);
+    }
+
     public Post() { }
-    public Post(String text, User user)
-    {
+
+    public Post(String text, User user) {
         this.text = text;
         this.user = user;
         this.postTime = new Date();
     }
 
-    public int getPost_id()
-    {
-        return post_id;
-    }
-    public void setPost_id(int post_id)
-    {
-        this.post_id = post_id;
-    }
-    public String getText()
-    {
-        return text;
-    }
-    public void setText(String text)
-    {
-        this.text = text;
-    }
-    public Date getPostTime()
-    {
-        return postTime;
-    }
-    public void setPostTime(Date postTime)
-    {
-        this.postTime = postTime;
-    }
-    public User getUser()
-    {
-        return user;
-    }
-    public void setUser(User user)
-    {
-        this.user = user;
-    }
-    public void addReply(Post post) {
-        replies.add(post);
-    }
-    public void removeReply(Post post) {
-        replies.remove(post);
-    }
-
-=======
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int post_id;
-
-	@Column
-	private String text;
-	
-	@Temporal(TemporalType.DATE)
-	private Date postTime;
-	
-	
-	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.PERSIST)
-	@JoinColumn(name="parent_id")
-	private Set<Post> replies;
-	
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@OnDelete(action = OnDeleteAction.CASCADE)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
-	
-	public Post() { }
-	public Post(String text, User user)
-	{
-		this.text = text;
-		this.user = user;
-		this.postTime = new Date();
-	}
-	
-	public int getPost_id()
-	{
-		return post_id;
-	}
-	public void setPost_id(int post_id)
-	{
-		this.post_id = post_id;
-	}
-	public String getText()
-	{
-		return text;
-	}
-	public void setText(String text)
-	{
-		this.text = text;
-	}
-	public Date getPostTime()
-	{
-		return postTime;
-	}
-	public void setPostTime(Date postTime)
-	{
-		this.postTime = postTime;
-	}
-	public User getUser()
-	{
-		return user;
-	}
-	public void setUser(User user)
-	{
-		this.user = user;
-	}
-	public void addReply(Post post) {
-		replies.add(post);
-	}
-	public void removeReply(Post post) {
-		replies.remove(post);
-	}
->>>>>>> 6e1ceb4b86e938fba730f32a878253ba68fdae79
 }
