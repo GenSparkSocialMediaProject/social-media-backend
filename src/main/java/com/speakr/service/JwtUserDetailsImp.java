ce54384bbe296d674a91533e6eb1a6687c17ceca
Alonso del Arte <alonso.delarte@gmail.com>
2022-08-17 17:53:54 -0400

    Dealing with my git diff mistake

--- src/main/java/com/speakr/service/JwtUserDetailsImp.java
+++ src/main/java/com/speakr/service/JwtUserDetailsImp.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Service
 public class JwtUserDetailsImp implements UserDetailsService
 {
-<<<<<<< HEAD
     final com.speakr.dao.UserDAO UserDAO;
     public JwtUserDetailsImp(com.speakr.dao.UserDAO userDAO) { this.UserDAO = userDAO; }
 
@@ -30,7 +29,7 @@ public class JwtUserDetailsImp implements UserDetailsService
             throw new UsernameNotFoundException(String.format("Username: \"%s\" does not exist.", username));
         }
         return new org.springframework.security.core.userdetails.User
-                (user.getUser_name(), user.getPassword(), authorityList);
+                (user.getUserName(), user.getPassword(), authorityList);
     }
 
 
@@ -42,32 +41,4 @@ public class JwtUserDetailsImp implements UserDetailsService
                 (username, password, authorityList);
     }
 
-=======
-	final com.speakr.dao.UserDAO UserDAO;
-	public JwtUserDetailsImp(com.speakr.dao.UserDAO userDAO) { this.UserDAO = userDAO; }
-	
-	
-	@Override
-	public UserDetails loadUserByUsername(String username)	 throws UsernameNotFoundException, NullPointerException
-	{
-		User user = UserDAO.findUserByUserName(username);
-		List<GrantedAuthority> authorityList = new ArrayList<>();
-		authorityList.add(new SimpleGrantedAuthority("USER_ROLE"));
-		System.out.println(UserDAO.findAll());
-		if (user == null) {
-			throw new UsernameNotFoundException(String.format("Username: \"%s\" does not exist.", username));
-		}
-		return new org.springframework.security.core.userdetails.User
-			(user.getUser_name(), user.getPassword(), authorityList);
-	}
-	
-	
-	public UserDetails createUserDetails(String username, String password)
-	{
-		List<GrantedAuthority> authorityList = new ArrayList<>();
-		authorityList.add(new SimpleGrantedAuthority("USER_ROLE"));
-		return new org.springframework.security.core.userdetails.User
-			(username, password, authorityList);
-	}
->>>>>>> 6e1ceb4b86e938fba730f32a878253ba68fdae79
 }
