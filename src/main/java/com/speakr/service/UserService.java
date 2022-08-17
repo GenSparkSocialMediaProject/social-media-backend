a78857a3f457f11d8a8b4bed948867b3364edc53
Alonso del Arte <alonso.delarte@gmail.com>
2022-08-17 18:00:09 -0400

    Dealing with my git diff mistake
    This file already existed but it got deleted at some point. Maybe that was my fault, I don't actually know.

---
+++ src/main/java/com/speakr/service/UserService.java
@@ -0,0 +1,20 @@
+package com.speakr.service;
+
+import com.speakr.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    User getUserById(int id);
+
+    User addUser(User user);
+    User addUser(String userName, String displayName, String bio);
+
+    User updateUser(User user);
+
+    User deleteUserById(int id);
+
+}
