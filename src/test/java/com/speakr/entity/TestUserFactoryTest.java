89bd0d2c944917c8a23d2c820b115740b9e95a2b
Alonso del Arte <alonso.delarte@gmail.com>
2022-08-18 01:25:18 -0400

    Restoring test class stubs lost under deadline pressure

---
+++ src/test/java/com/speakr/entity/TestUserFactoryTest.java
@@ -0,0 +1,18 @@
+package com.speakr.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestUserFactoryTest {
+
+
+    @Test
+    void testCreateNewUser() {
+        System.out.println("createNewUser");
+        User user = TestUserFactory.createNewUser();
+        assert user.getUserName() != null : "User name should not be null";
+        assert user.getDisplayName() != null
+                : "Display name should not be null";
+    }
+
+}
